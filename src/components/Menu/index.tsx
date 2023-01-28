0bd0b2eef4cf8ff7a59ae8ff2671be8ebce31e37
Michael Tran <mtran7@ualberta.ca>
2023-01-27 23:59:19 -0800

    made button

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,12 @@
+import { IconButton } from "@mui/material";
+import { FiMenu } from "react-icons/fi";
+
+const Component = () => {
+  return (
+    <IconButton aria-label="menu" color="secondary" size="large">
+      <FiMenu />
+    </IconButton>
+  );
+};
+
+export default Component;
